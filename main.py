f0056c343a0fa6a89f05ba1d5b713bd57011f428
jesterb0206 <jesterb@seattleu.edu>
2023-06-10 23:26:03 -0700

    Source Code for Mathematical Operations Quiz

---
+++ main.py
@@ -0,0 +1,45 @@
+# If the bill was $150.00, split between 5 people, with 12% tip.
+
+# Each person should pay (150.00 / 5) * 1.12 = 33.6
+# Format the result to 2 decimal places = 33.60
+
+# Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+# Write your code below this line 👇
+
+print("Welcome to the tip calculator.")
+
+total = float(input("What was the total bill? $"))
+
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+
+people = int(input("How many people should the bill be split between? "))
+
+answer = (total / people) * (tip / 100 + 1)
+
+bill = str(round(answer, 2))
+
+print(f"Each person should pay: ${bill}")
+
+# Angela's Solution
+
+# If the bill was $150.00, split between 5 people, with 12% tip.
+# Each person should pay (150.00 / 5) * 1.12 = 33.6
+# Round the result to 2 decimal places.
+
+# print("Welcome to the tip calculator!")
+# bill = float(input("What was the total bill? $"))
+# tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+# people = int(input("How many people to split the bill?"))
+
+# tip_as_percent = tip / 100
+# total_tip_amount = bill * tip_as_percent
+# total_bill = bill + total_tip_amount
+# bill_per_person = total_bill / people
+# final_amount = round(bill_per_person, 2)
+
+# FAQ: How to round to 2 decimal places?
+
+# Find the answer in the Q&A here: https://www.udemy.com/course/100-days-of-code/learn/lecture/17965132#questions/1331504
+
+# print(f"Each person should pay: ${final_amount}")
